04e21b5189ab052b8610710456874c7b81796ecf
dailydevp <hyde8547@naver.com>
2021-03-25 11:16:41 +0900

    first commit

---
+++ src/p01/Comment.java
@@ -0,0 +1,17 @@
+package p01;
+
+public class Comment {
+	public static void main(String[] args) {
+		System.out.println("주석");
+		
+		//한 줄 주석은 slash 두
+		System.out.println("한 줄 주석");
+		
+	/*
+	 * 여러줄 주석은slash 별 ~~ 별 slash 까
+	 */
+	
+		System.out.println("여러 줄 주석");
+	}
+}
+
---
+++ src/p01/Hello.java
@@ -0,0 +1,7 @@
+package p01;
+
+public class Hello {
+	public static void main(String arg[]) {
+		System.out.println("Hello World");
+	}
+}
