f5ac71f50c5a9974f7a4795553144e35bf3deba6
dailydevp <hyde8547@naver.com>
2021-04-02 22:15:18 +0900

    first

--- src/p05/lecture/A19ArrayInitValue.java
+++ src/p05/lecture/A19ArrayInitValue.java
@@ -1,5 +1,15 @@
 package p05.lecture;
 
 public class A19ArrayInitValue {
-
+	public static void main(String[] args) {
+		int[] arr1 = new int[3];
+		int[] arr2 = new int[] {55, 66, 77};
+		int[] arr3 = {3, 4, 5, 6, 7};
+		
+		int[] arr4;
+		arr4 = new int[] {9, 8, 7}; // ok
+		
+		int[] arr5;
+//		arr5 = {34, 56, 78}; // x
+	}
 }
