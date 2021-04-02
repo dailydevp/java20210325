ebd68817c588838ba15e6ef1f425f4e275de31b8
dailydevp <hyde8547@naver.com>
2021-04-02 23:49:04 +0900

    first

--- src/p05/textbook/ArrayCreateByNewExample.java
+++ src/p05/textbook/ArrayCreateByNewExample.java
@@ -15,16 +15,24 @@ public class ArrayCreateByNewExample {
 		
 		double[] arr2 = new double[3];
 		for(int i =0; i<3; i++) {
+			System.out.println("arr2["+i+"] : " + arr2[i]);	
+		}
+			arr2[0] = 0.1;
+			arr2[1] = 0.2;
+			arr2[2] = 0.3;
+		for(int i =0; i<3; i++) {
 			System.out.println("arr2[" + i + "] :" + arr2[i]);
 		}
 		
-		arr2[0] = 0.1;
-		arr2[1] = 0.2;
-		arr2[2] = 0.3;
-		for(int i =0; i<3; i++) {
-			System.out.println();
-			not yet
+		String[] arr3 = new String[3];
+		for(int i = 0; i<3; i++) {
+			System.out.println("arr3["+i+"] : " + arr3[i]);
 		}
-		
+		arr3[0] = "1월";
+		arr3[1] = "2월";
+		arr3[2] = "3월";
+		for(int i =0; i<3; i++) {
+			System.out.println("arr3["+i+"] :" + arr3[i]);
+		}		
 	}
 }
