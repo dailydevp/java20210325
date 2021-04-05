4ec2ee92b04cc3571d9405c40d2c4bec67730cc2
dailydevp <hyde8547@naver.com>
2021-04-05 23:28:40 +0900

    first

--- src/p05/textbook/StringEqualsExample.java
+++ src/p05/textbook/StringEqualsExample.java
@@ -4,7 +4,7 @@ public class StringEqualsExample {
 	public static void main(String[] args) {
 		String strVar1 = "신민철";
 		String strVar2 = "신민철";
-		
+		 
 		if(strVar1 == strVar2) {
 			System.out.println("strVar1과 strVar2는 참조가 같음");
 		}else {
